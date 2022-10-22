fef98568c06fee2ff68234b3bf47459dc9602ca0
RobledoJohn <robledoJohn@hotmail.com>
2022-10-21 21:32:51 -0500

    creando carpeta resources

---
+++ src/components/perfil.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+export function Profile() {
+    
+    return(
+        <Fragment>
+            <div id="container">
+                <h1>Aqui esta la prueba de perfil</h1>
+            </div>
+        </Fragment>
+    )
+
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; 
+import { Profile } from './components/perfil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Profile/>
   </React.StrictMode>
 );
 
