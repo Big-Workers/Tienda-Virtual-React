222d7c41546b67ceaa568b755e83afb587ef2a9b
Deivid-Ruales <davidral33@hotmail.com>
2022-10-23 19:34:51 -0500

    Creación header.js y header.css

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,27 @@
 import '../styles/styleHeader.css';
-import cliente from '..//resources/usuario.png';
+import usuario from '../resources/usuario.png';
+import logo from '../resources/logo-1.png';
+import carrito from '../resources/carrito-de-compras.png';
+import productos from '../resources/paquete.png';
+import home from '../resources/casa-de-perro.png';
 
 export function Header () {
     return (
-        <>
-            <div id="divHeader">
-                <h1>ARTESANIA</h1>
-                <div id="divUsuario">
-                    <img src={cliente} className="Logo-cliente" alt="logoUsuario" />
-                </div>
+        <nav className="header">
+            <a href="#"><img src={logo} className="logo" alt="logo" /></a>
+            <div className="divFondoIconos" id="fondoUsuario">
+                <a href="#"><img src={usuario} className="iconosHeader" alt="Perfil"/></a>
             </div>
-        </>
+            <div className="divFondoIconos" id="fondoCarrito">
+                <a href="#"><img src={carrito} className="iconosHeader" alt="CarritoCompras"/></a>
+            </div>
+            <div className="divFondoIconos" id="fondoProductos">
+                <a href="#"><img src={productos} className="iconosHeader" alt="Productos"/></a>
+            </div>        
+            <div className="divFondoIconos" id="fondoProductos">
+                <a href="#"><img src={home} className="iconosHeader" alt="Home"/></a>
+            </div> 
+        </nav>
 
     )
 }
