4d9d77093c42e0477f07dc8040a93d52e50e241c
Deivid-Ruales <davidral33@hotmail.com>
2022-10-23 23:45:18 -0500

    Creación header y footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import { Header } from './components/header.js';
-
 
 
 function App() {
   return (
-    /*<div className="App">
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,10 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>*/
-    <>
-      <Header/>
-    </>
+    </div>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import '../styles/styleHeader.css';
+import '../styles/header.css';
 import usuario from '../resources/usuario.png';
 import logo from '../resources/logo-1.png';
 import carrito from '../resources/carrito-de-compras.png';
@@ -12,13 +12,13 @@ export function Header () {
             <div className="divFondoIconos" id="fondoUsuario">
                 <a href="#"><img src={usuario} className="iconosHeader" alt="Perfil"/></a>
             </div>
-            <div className="divFondoIconos" id="fondoCarrito">
+            <div className="divFondoIconos">
                 <a href="#"><img src={carrito} className="iconosHeader" alt="CarritoCompras"/></a>
             </div>
-            <div className="divFondoIconos" id="fondoProductos">
+            <div className="divFondoIconos">
                 <a href="#"><img src={productos} className="iconosHeader" alt="Productos"/></a>
             </div>        
-            <div className="divFondoIconos" id="fondoProductos">
+            <div className="divFondoIconos">
                 <a href="#"><img src={home} className="iconosHeader" alt="Home"/></a>
             </div> 
         </nav>
