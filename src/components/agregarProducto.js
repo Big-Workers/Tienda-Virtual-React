a7b5e158f40d95cf0f8fb7ed66d7c871e4aa0874
RobledoJohn <robledoJohn@hotmail.com>
2022-11-06 11:53:35 -0500

    corrigiendo vista registro

--- src/components/agregarProducto.js
+++ src/components/agregarProducto.js
@@ -1,39 +1,44 @@
 import { Fragment } from "react";
 import { Component } from "react";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "../styles/agregarProducto.css";
 
 const inventarioVacio = [
   {
-    "id": null,
-    "ref": "",
-    "img": "",
-    "Name": "",
-    "Descripcion": "",
-    "Precio": null,
-    "Inventario": null,
-    "vendidos": null
-  }];
+    id: null,
+    ref: "",
+    img: "",
+    Name: "",
+    Descripcion: "",
+    Precio: null,
+    Inventario: null,
+    vendidos: null,
+  },
+];
 
-export const AgregarProducto = ({ prod, setDataToEditProd, crearProducto, actualizarProducto }) => {
+export const AgregarProducto = ({
+  prod,
+  setDataToEditProd,
+  crearProducto,
+  actualizarProducto,
+}) => {
   const [invent, setInvent] = useState(inventarioVacio);
 
   const Cambio = (e) => {
     setInvent({
       ...invent,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const Submit = (e) => {
     e.preventDefault();
 
-
     if (invent.id === null) {
       crearProducto(invent);
     } else {
       actualizarProducto(invent);
-    };
+    }
 
     Reset();
   };
@@ -41,67 +46,78 @@ export const AgregarProducto = ({ prod, setDataToEditProd, crearProducto, actual
   const Reset = (e) => {
     setInvent(inventarioVacio);
     setDataToEditProd(null);
-
-  }
+  };
   return (
-    <>
+    <Fragment>
       <center>
         <div class="formato">
           <h2 class="titulo-formato">AGREGAR PRODUCTO</h2>
-          <form class="format" onSubmit={Submit}>
-            <label for="Nombre del Producto"></label>
+          <form class="format">
+            <label for="nombre-Producto"></label>
             <input
               class="campo"
               type="text"
-              name="Nombre del Producto"
+              name="nombre-producto"
               placeholder="Nombre del Producto"
-              onChange={Cambio} value={invent.Name}
+              onChange={Cambio}
+              value={invent.Name} //
             />
-            <br></br>
-            <label for="Nonbre del Producto"></label>
+            <label for="referencia"></label>
             <input
               class="campo"
               type="text"
-              name="Nombre del Producto"
+              name="referencia"
+              placeholder="Referencia"
+            />
+            <label for="stock"></label>
+            <input
+              class="campo"
+              type="text"
+              name="stock"
+              placeholder="Stock"
+              onChange={Cambio}
+              value={invent.Inventario} //
+            />
+            <label for="valor"></label>
+            <input
+              class="campo"
+              type="text"
+              name="valor"
+              placeholder="Valor"
+              onChange={Cambio}
+              value={invent.Precio}
+            />
+            <label for="descripcion"></label>
+            <input
+              class="campo"
+              type="text"
+              name="descripcion"
               placeholder="Descripción"
-              onChange={Cambio} value={invent.Descripcion}
+              onChange={Cambio}
+              value={invent.Descripcion} //
             />
-            <br></br>
-            <label for="Nonbre del Producto"></label>
-            <input
-              class="campo"
-              type="text"
-              name="Nombre del Producto"
-              placeholder="Cantidad en Stock"
-              onChange={Cambio} value={invent.Inventario}
-            />
-            <br></br>
-            <label for="Nonbre del Producto"></label>
-            <input
-              class="campo"
-              type="text"
-              name="Nombre del Producto"
-              placeholder="Precio Unitario de venta"
-              onChange={Cambio} value={invent.Precio}
-            />
-            <br></br>
-            <label for="Nonbre del Producto"></label>
+            <label for="agregar-imagen"></label>
             <input
               class="Seleccion"
               type="file"
-              name="Nombre del Producto"
+              name="agregar-imagen"
               placeholder="Añadir Imagenes"
-              onChange={Cambio} value={invent.img}
+              onChange={Cambio}
+              value={invent.img}
             />
             <div class="Boton-agregar-producto">
-              <a type="submit" href="/Admin-Productos" onClick={Submit} className="bton elemento" >
+              <a
+                type="submit"
+                href="/Admin-Productos"
+                onClick={Submit}
+                className="bton elemento"
+              >
                 Ingresar
               </a>
             </div>
           </form>
         </div>
       </center>
-    </>
+    </Fragment>
   );
 };
-
--- src/components/registro.js
+++ src/components/registro.js
@@ -9,39 +9,39 @@ export class Registro extends Component {
         <center>
           <div class="contenedor-login">
             <div class="formulario">
-              <h2 class="titulo-formulario">Registrarse</h2>
-              <form class="formatito">
+              <h2 class="titulo-formulario-reg">Registrarse</h2>
+              <form class="formatito-reg">
                 <label for="usuario o email"></label>
                 <input
-                  class="campotex"
+                  class="campo-tex"
                   type="text"
                   name="usuario o email"
                   placeholder="Nombre"
                 />
                 <label for="correo"></label>
                 <input
-                  class="campotex"
+                  class="campo-tex"
                   type="text"
                   name="correo"
                   placeholder="Correo"
                 />
                 <label for="Telefono"></label>
                 <input
-                  class="campotex"
+                  class="campo-tex"
                   type="tel"
                   name="Telefono"
                   placeholder="Telefono"
                 />
                 <label for="Direccion"></label>
                 <input
-                  class="campotex"
+                  class="campo-tex"
                   type="text"
                   name="Dirección"
                   placeholder="Direccion"
                 />
                 <label for="usuario o email"></label>
                 <input
-                  class="campotex"
+                  class="campo-tex"
                   type="text"
                   name="usuario o email"
                   placeholder="Nombre"
@@ -49,7 +49,7 @@ export class Registro extends Component {
               </form>
               <div>
                 <button onclick="btn" className="btn element">
-                  <a href="/Home">Registrarse</a>
+                  <a class="line" href="/Home">Registrarse</a>
                 </button>
               </div>
               <div className="pass">
